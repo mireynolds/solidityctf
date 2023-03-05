0d204e59c6e21c2bafe3de62daff69ff37e350ee
Michael Reynolds <103044686+mireynolds@users.noreply.github.com>
2023-03-05 17:52:56 +0000

    Updated to deal with larger nonces

--- level2/level2funsolution_helper.sol
+++ level2/level2funsolution_helper.sol
@@ -1,14 +1,20 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract AddressCalculator {
-    function calculateAddress(address deployingAddress, uint256 nonce) public pure returns (address, address) {
-        bytes32 hash = keccak256(abi.encodePacked(
-            bytes1(0xff),
-            deployingAddress,
-            bytes32(nonce)
-        ));
-        address c1 = address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), deployingAddress, bytes1(uint8(nonce)))))));
-        address c2 = address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), c1, bytes1(uint8(1)))))));
-        return (c1, c2);
+    function calculateAddresses(address deployingAddress, uint64 nonce) public pure returns (address, address) {
+        address contract1 = getAddress(deployingAddress, nonce);
+        address contract2 = getAddress(contract1, 1);
+        return (contract1, contract2);
+    }
+    function getAddress(address addr, uint nonce) internal pure returns (address) {
+        bytes memory data;
+        if (nonce == 0x00)          data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), addr, bytes1(0x80));
+        else if (nonce <= 0x7f)     data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), addr, uint8(nonce));
+        else if (nonce <= 0xff)     data = abi.encodePacked(bytes1(0xd7), bytes1(0x94), addr, bytes1(0x81), uint8(nonce));
+        else if (nonce <= 0xffff)   data = abi.encodePacked(bytes1(0xd8), bytes1(0x94), addr, bytes1(0x82), uint16(nonce));
+        else if (nonce <= 0xffffff) data = abi.encodePacked(bytes1(0xd9), bytes1(0x94), addr, bytes1(0x83), uint24(nonce));
+        else                         data = abi.encodePacked(bytes1(0xda), bytes1(0x94), addr, bytes1(0x84), uint32(nonce));
+        return address(uint160(uint256(keccak256(data))));
     }
 }
